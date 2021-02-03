ff483f553d5cfa59ae1caf4c5845fc92f292ad48
陳鈞 <jim60105@gmail.com>
2021-02-04 06:50:52 +0800

    使用編譯時期環境變數

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,6 @@
+declare const process: any;
+const SOMEENV: string = process.env.SOMEENV;
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  return new Response(`Environment Variable: ${SOMEENV}`)
 }
